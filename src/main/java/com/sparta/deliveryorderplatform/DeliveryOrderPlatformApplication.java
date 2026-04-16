c83a8c4bae9839047fd200877477ea0f6650837f
tyang0123 <84624287+tyang0123@users.noreply.github.com>
2026-04-16 19:23:30 +0900

    init commit

---
+++ src/main/java/com/sparta/deliveryorderplatform/DeliveryOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.deliveryorderplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryOrderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryOrderPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/deliveryorderplatform/DeliveryOrderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deliveryorderplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryOrderPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
