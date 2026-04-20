d0b0bd057b9f91d0a7aa08111dfdefb776eaf529
김승현 <shkim7020@gmail.com>
2026-04-20 21:13:31 +0900

    fix: PaymentRepository 제네릭 타입 수정

--- src/main/java/com/sparta/deliveryorderplatform/payment/repository/PaymentRepository.java
+++ src/main/java/com/sparta/deliveryorderplatform/payment/repository/PaymentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface PaymentRepository extends JpaRepository<UUID, Payment> {
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
