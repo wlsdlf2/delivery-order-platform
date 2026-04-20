7d9ee1c3c4954e8a803abb4054227ff4818da7d3
김승현 <shkim7020@gmail.com>
2026-04-20 21:05:25 +0900

    add : CreatePaymentRequest dto 클래스 생성

---
+++ src/main/java/com/sparta/deliveryorderplatform/payment/dto/request/CreatePaymentRequest.java
@@ -0,0 +1,14 @@
+package com.sparta.deliveryorderplatform.payment.dto.request;
+
+import com.sparta.deliveryorderplatform.payment.entity.PaymentMethod;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreatePaymentRequest {
+
+    @NotNull
+    private PaymentMethod PaymentMethod;
+}
