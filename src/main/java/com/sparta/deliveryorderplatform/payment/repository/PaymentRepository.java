3571edd2969fb64625e22911e4fe911b232a910f
김승현 <shkim7020@gmail.com>
2026-04-20 18:55:07 +0900

    add : PaymentRepository 생성

---
+++ src/main/java/com/sparta/deliveryorderplatform/payment/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.deliveryorderplatform.payment.repository;
+
+import com.sparta.deliveryorderplatform.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PaymentRepository extends JpaRepository<UUID, Payment> {
+}
