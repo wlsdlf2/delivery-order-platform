a6b7e80090588274d9b0343f490c016dd424aea9
김승현 <shkim7020@gmail.com>
2026-04-20 18:53:49 +0900

    add : Payment 엔티티 생성

---
+++ src/main/java/com/sparta/deliveryorderplatform/payment/entity/Payment.java
@@ -0,0 +1,42 @@
+package com.sparta.deliveryorderplatform.payment.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "p_payment")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID orderId;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private PaymentMethod paymentMethod = PaymentMethod.CARD;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus = PaymentStatus.PENDING;
+
+    private Integer amount;
+
+    // audit field
+
+    public static Payment create(UUID orderId, Integer amount) {
+        return Payment.builder()
+                .orderId(orderId)
+                .amount(amount)
+                .build();
+    }
+}
