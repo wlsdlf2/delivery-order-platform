1908fc7a47f3d287d2f1b4b475eae13f3b8cee69
mindy <92miindy@gmail.com>
2026-04-23 00:39:47 +0900

    feat : 운영지역 다건 조회, 1건 상세조회 기능 구현

--- src/main/java/com/sparta/deliveryorderplatform/area/repository/AreaRepository.java
+++ src/main/java/com/sparta/deliveryorderplatform/area/repository/AreaRepository.java
@@ -2,12 +2,11 @@ package com.sparta.deliveryorderplatform.area.repository;
 
 import com.sparta.deliveryorderplatform.area.entity.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface AreaRepository extends JpaRepository<Area, UUID> {
+public interface AreaRepository extends JpaRepository<Area, UUID>, AreaRepositoryCustom {
     boolean existsByNameAndDeletedAtIsNull(String name);
 
     Optional<Area> findByIdAndDeletedAtIsNull(UUID id);
--- src/main/java/com/sparta/deliveryorderplatform/area/service/AreaService.java
+++ src/main/java/com/sparta/deliveryorderplatform/area/service/AreaService.java
@@ -2,11 +2,14 @@ package com.sparta.deliveryorderplatform.area.service;
 
 import com.sparta.deliveryorderplatform.area.dto.AreaRequestDTO;
 import com.sparta.deliveryorderplatform.area.dto.AreaResponseDTO;
+import com.sparta.deliveryorderplatform.area.dto.AreaSearchDTO;
 import com.sparta.deliveryorderplatform.area.entity.Area;
 import com.sparta.deliveryorderplatform.area.repository.AreaRepository;
 import com.sparta.deliveryorderplatform.global.exception.CustomException;
 import com.sparta.deliveryorderplatform.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +24,6 @@ public class AreaService {
     // create
     @Transactional
     public AreaResponseDTO createArea(AreaRequestDTO requestDTO) {
-        // 운영지역명 중복 체크
         validateDuplicateName(requestDTO.getName());
 
         Area area = Area.create(requestDTO.getName(), requestDTO.getCity(), requestDTO.getDistrict());
@@ -29,17 +31,27 @@ public class AreaService {
         return AreaResponseDTO.from(savedArea);
     }
 
-
-
     // get 목록 조회
+    @Transactional(readOnly = true)
+    public Page<AreaResponseDTO> getAreas(AreaSearchDTO searchDTO, String role, Pageable pageable) {
+        // 관리자 여부 판단
+        boolean isAdmin = "MASTER".equals(role);
+        searchDTO.setIsAdmin(isAdmin);
+
+        return areaRepository.searchAreas(searchDTO, pageable).map(AreaResponseDTO::from);
+    }
 
     // get 상세 조회
+    @Transactional(readOnly = true)
+    public AreaResponseDTO getAreaById(UUID areaId) {
+        Area area = findAreaById(areaId);
+        return AreaResponseDTO.from(area);
+    }
 
     // update
     @Transactional
     public AreaResponseDTO updateArea(UUID areaId, AreaRequestDTO requestDTO) {
-        // 삭제되지 않은 데이터 조회
-        Area area = findActiveArea(areaId);
+        Area area = findAreaById(areaId);
 
         // 지역명이 변경될 때만 운영지역명 중복 체크
         if (!area.getName().equals(requestDTO.getName())) {
@@ -51,19 +63,21 @@ public class AreaService {
     }
 
     // delete
+    // todo store 기능 구현 시, 연관관계가 있는 경우 삭제되지 않도록 수정 필요
     @Transactional
     public AreaResponseDTO deleteArea(UUID areaId, String username) {
-        // 삭제되지 않은 데이터 조회
-        Area area = findActiveArea(areaId);
+        Area area = findAreaById(areaId);
         area.delete(username);
         return AreaResponseDTO.from(area);
     }
 
-    private Area findActiveArea(UUID areaId) {
+    // 헬퍼 메서드: 삭제되지 않은 데이터 조회
+    private Area findAreaById(UUID areaId) {
         return areaRepository.findByIdAndDeletedAtIsNull(areaId)
             .orElseThrow(() -> new CustomException(ErrorCode.AREA_NOT_FOUND));
     }
 
+    // 헬퍼 메서드: 지역명 중복 조회
     private void validateDuplicateName(String name) {
         if (areaRepository.existsByNameAndDeletedAtIsNull(name)) {
             throw new CustomException(ErrorCode.DUPLICATE_AREA_NAME);
