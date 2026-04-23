c58fba6dc8ccb8c48a81925b77f56061cc60f159
wlsdlf2 <wlsdlfzg@naver.com>
2026-04-23 12:10:43 +0900

    chore: indent 맞춤 및 주석 추가

--- src/main/java/com/sparta/deliveryorderplatform/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/deliveryorderplatform/global/exception/ErrorCode.java
@@ -6,21 +6,23 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-	// 400 BAD_REQUEST(예시)
+	// auth
 	VALIDATION_ERROR(400, "VALIDATION_ERROR", "요청 값이 올바르지 않습니다."),
 	LOGIN_FAILED(400, "LOGIN_FAILED", "아이디 또는 비밀번호가 일치하지 않습니다."),
 	DUPLICATE_USERNAME(400, "DUPLICATE_USERNAME", "이미 존재하는 아이디입니다."),
 	DUPLICATE_EMAIL(400, "DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다."),
 	INVALID_ROLE_SELECTION(400, "INVALID_ROLE_SELECTION", "가입할 수 없는 권한입니다."),
+  	UNAUTHORIZED_ACCESS(403, "UNAUTHORIZED_ACCESS", "접근 권한이 없습니다."),
 
-  UNAUTHORIZED_ACCESS(403, "UNAUTHORIZED_ACCESS", "접근 권한이 없습니다."),
+	// category
+  	DUPLICATE_CATEGORY_NAME(400, "DUPLICATE_CATEGORY_NAME", "이미 존재하는 카테고리명입니다."),
+  	CATEGORY_NOT_FOUND(404, "CATEGORY_NOT_FOUND", "존재하지 않거나 삭제된 카테고리입니다."),
 
-  DUPLICATE_CATEGORY_NAME(400, "DUPLICATE_CATEGORY_NAME", "이미 존재하는 카테고리명입니다."),
-  CATEGORY_NOT_FOUND(404, "CATEGORY_NOT_FOUND", "존재하지 않거나 삭제된 카테고리입니다."),
-
+	// area
 	DUPLICATE_AREA_NAME(400, "DUPLICATE_AREA_NAME", "이미 존재하는 운영지역명입니다."),
 	AREA_NOT_FOUND(404, "AREA_NOT_FOUND", "존재하지 않거나 삭제된 운영지역입니다."),
 
+	// store
 	STORE_NOT_FOUND(404, "STORE_NOT_FOUND", "존재하지 않거나 삭제된 가게입니다."),
 
 	// payment
@@ -29,7 +31,7 @@ public enum ErrorCode {
 	ALREADY_CANCELLED(400, "ALREADY_CANCELLED", "이미 취소된 결제입니다."),
 	INVALID_PAYMENT_STATUS(400, "INVALID_PAYMENT_STATUS", "유효하지 않은 결제 상태 변경입니다."),
 
-	// 401 Unauthorized
+	// jwt
 	INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
 	EXPIRED_TOKEN(401, "EXPIRED_TOKEN", "만료된 토큰입니다."),
 	UNSUPPORTED_TOKEN(401, "UNSUPPORTED_TOKEN", "지원되지 않는 토큰입니다."),
