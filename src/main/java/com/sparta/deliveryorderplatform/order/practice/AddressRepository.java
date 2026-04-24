8f84373a2f99ba6b19d7657a25c55cc372c2db15
jun-geun-choi <jklucky73@gmail.com>
2026-04-24 14:17:11 +0900

    2500424-14:16-코드 점검 후 주석 처리하여 올립니다.

--- src/main/java/com/sparta/deliveryorderplatform/order/practice/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.deliveryorderplatform.order.practice;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, UUID> {
-}
--- src/main/java/com/sparta/deliveryorderplatform/order/practice/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.deliveryorderplatform.order.practice;
-
-import com.sparta.deliveryorderplatform.store.entity.Store;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, UUID> {
-}
