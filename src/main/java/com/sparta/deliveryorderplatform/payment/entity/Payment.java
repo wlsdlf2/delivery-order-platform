5c25caca531da4961149894827a08cbd87a64b84
김승현 <shkim7020@gmail.com>
2026-04-28 14:50:19 +0900

    fix: 주문 1:1 결제 unique 제약 조건 추가

--- src/main/java/com/sparta/deliveryorderplatform/payment/entity/Payment.java
+++ src/main/java/com/sparta/deliveryorderplatform/payment/entity/Payment.java
@@ -27,7 +27,7 @@ public class Payment extends BaseAuditEntity {
     private UUID id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id", nullable = false)
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
     private Order order;
 
     @Builder.Default
